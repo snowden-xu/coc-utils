c2dfc6907d0901b21765ac087616e123e0fc099b
snowden.xu <snowden.xu@gmail.com>
2023-07-25 21:50:56 +0800

    feat: 使用tsconfig输出ts声明文件

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare function random(num: number): number;
-
-export { random };
--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "rollup";
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+// import dts from "rollup-plugin-dts";
 
 const config = defineConfig([
   // 配置输出ES Module
@@ -12,15 +12,15 @@ const config = defineConfig([
     },
     plugins: [typescript()],
   },
-  // 配置打包类型声明
-  {
-    input: "src/index.ts",
-    output: {
-      file: "dist/index.d.ts",
-      format: "es",
-    },
-    plugins: [dts()],
-  },
+  // // 配置打包类型声明
+  // {
+  //   input: "src/index.ts",
+  //   output: {
+  //     file: "dist/index.d.ts",
+  //     format: "es",
+  //   },
+  //   plugins: [dts()],
+  // },
 ]);
 
 export default config;
