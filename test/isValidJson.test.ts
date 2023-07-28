8bf139dd5bd5b7e334a76e23e0c833c7f97251ad
snowden.xu <snowden.xu@mobilemd.cn>
2023-07-28 16:15:05 +0800

    feat: 优化代码

--- test/isValidJson.test.ts
+++ test/isValidJson.test.ts
@@ -1,36 +1,35 @@
-import { isValidJson } from '../src/isValidJson';
+import { isValidJson } from "../src";
 
+describe("isJsonString", () => {
+  // 测试当传入的参数为有效的JSON对象时，应该返回true
+  it("should return true for valid JSON object", () => {
+    // 定义一个有效的JSON对象
+    const jsonStr = '{"name": "John", "age": 30, "city": "New York"}';
+    // 断言当传入的参数为有效的JSON对象时，应该返回true
+    expect(isValidJson(jsonStr)).toBe(true);
+  });
 
-describe('isJsonString', () => {
-    // 测试当传入的参数为有效的JSON对象时，应该返回true
-    it('should return true for valid JSON object', () => {
-        // 定义一个有效的JSON对象
-        const jsonStr = '{"name": "John", "age": 30, "city": "New York"}';
-        // 断言当传入的参数为有效的JSON对象时，应该返回true
-        expect(isValidJson(jsonStr)).toBe(true);
-    });
+  // 测试当传入的参数为有效的JSON数组时，应该返回true
+  it("should return true for valid JSON array", () => {
+    // 定义一个有效的JSON数组
+    const jsonStr = "[1, 2, 3]";
+    // 断言当传入的参数为有效的JSON数组时，应该返回true
+    expect(isValidJson(jsonStr)).toBe(true);
+  });
 
-    // 测试当传入的参数为有效的JSON数组时，应该返回true
-    it('should return true for valid JSON array', () => {
-        // 定义一个有效的JSON数组
-        const jsonStr = '[1, 2, 3]';
-        // 断言当传入的参数为有效的JSON数组时，应该返回true
-        expect(isValidJson(jsonStr)).toBe(true);
-    });
+  // 测试当传入的参数为无效的JSON时，应该返回false
+  it("should return false for invalid JSON with trailing comma", () => {
+    // 定义一个无效的JSON
+    const jsonStr = '{"name": "John", "age": 30, "city": "New York",}';
+    // 断言当传入的参数为无效的JSON时，应该返回false
+    expect(isValidJson(jsonStr)).toBe(false);
+  });
 
-    // 测试当传入的参数为无效的JSON时，应该返回false
-    it('should return false for invalid JSON with trailing comma', () => {
-        // 定义一个无效的JSON
-        const jsonStr = '{"name": "John", "age": 30, "city": "New York",}';
-        // 断言当传入的参数为无效的JSON时，应该返回false
-        expect(isValidJson(jsonStr)).toBe(false);
-    });
-
-    // 测试当传入的参数为无效的JSON时，应该返回false
-    it('should return false for invalid JSON', () => {
-        // 定义一个无效的JSON
-        const jsonStr = 'not a valid JSON';
-        // 断言当传入的参数为无效的JSON时，应该返回false
-        expect(isValidJson(jsonStr)).toBe(false);
-    });
+  // 测试当传入的参数为无效的JSON时，应该返回false
+  it("should return false for invalid JSON", () => {
+    // 定义一个无效的JSON
+    const jsonStr = "not a valid JSON";
+    // 断言当传入的参数为无效的JSON时，应该返回false
+    expect(isValidJson(jsonStr)).toBe(false);
+  });
 });
