4add187aa4db1d82b33c631b2f45be5bc5ee6733
snowden.xu <snowden.xu@mobilemd.cn>
2023-07-28 17:20:31 +0800

    perf: 优化打包配置

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,29 +1,39 @@
 import { defineConfig } from "rollup";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import commonjs from '@rollup/plugin-commonjs';
-import clear from 'rollup-plugin-clear';
+import commonjs from "@rollup/plugin-commonjs";
+import clear from "rollup-plugin-clear";
 
 const config = defineConfig([
-  // 配置输出ES Module
+  // 配置输出ES Module 和 CommonJS
   {
     input: "src/index.ts",
-    output: {
-      file: "dist/index.es.js",
-      format: "es",
-    },
-    plugins: [typescript()],
+    output: [
+      {
+        file: "dist/index.esm.js",
+        format: "esm",
+      },
+      // {
+      //   dir: "dist/index.cjs.js",
+      //   format: "cjs",
+      // },
+    ],
+    plugins: [
+      typescript(),
+      commonjs(),
+      clear({
+        targets: ["dist"],
+      }),
+    ],
   },
   // 配置打包类型声明
   {
     input: "src/index.ts",
     output: {
       file: "dist/index.d.ts",
-      format: "es",
+      format: "esm",
     },
-    plugins: [commonjs(),dts(),clear({
-      targets: ['dist'],
-    })],
+    plugins: [dts()],
   },
 ]);
 
